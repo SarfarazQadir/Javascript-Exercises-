1c69a840949453e4ddfd66b7f3d153fa20abe667
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-11-16 08:51:21 +0500

    first

---
+++ app.js
@@ -0,0 +1,68 @@
+//  Assignment 26 to 30
+
+//  1
+
+// var user = +prompt();
+
+// document.write("number",user);
+// document.write("round of value : ",Math.round(user));
+// document.write("FLOOR of value : ",Math.floor(user));
+// document.write("ciel of value : ",Math.ceil(user));
+
+//  2
+
+//  var user = -prompt();
+
+// document.write("number",user);
+// document.write("<br>round of value : ",Math.round(user));
+// document.write("<br>FLOOR of value : ",Math.floor(user));
+// document.write("<br>ciel of value : ",Math.ceil(user));
+
+
+// 3
+
+// var num = -4
+// document.write("The value of absolute is : " + Math.abs(num));
+
+//4
+
+// var a = Math.random(1)*6;
+// var b = a.toFixed();
+// document.write("Randome Dice Number is : "+b)
+
+//5
+
+// var a = Math.random(1)*2;
+// var b = a.toFixed();
+// if(b == 2){
+//     document.write("Randome coin value is : Heads");
+// }else{
+//     document.write("Randome coin value is : Tails");
+// }
+
+//6
+
+// var a = Math.random()*10;
+// document.write("random number between 0 and 10 is : ",a);
+// document.write(a);
+
+//7
+
+// var user = prompt("type your wieght");
+
+// document.write(user.toFix);
+// document.write(Math.round(user),"kgs");
+// document.write(Math.floor(user),"kgs");
+// document.write(Math.ceil(user),"kgs");
+
+//8
+
+// var sec = 10;
+
+// var user = prompt("guess the secret number");
+
+// if(user == sec){
+//     alert("congratulation" +sec+ " you guessed it");
+// }else{
+//     alert("sorry you guessed it wrong");
+// }
